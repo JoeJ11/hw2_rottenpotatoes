9adf9d82d46bdb16391adf8ce1ba8b4767ee649c
= <=>
2013-10-20 16:53:21 +0800

    P3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,31 +10,53 @@ class MoviesController < ApplicationController
     @movies = []
     @rating_flag = {}
 	@all_ratings = Movie.get_all_ratings
+    redirect_flag = false
+
+    if params.has_key?(:data_package)
+      list = params[:data_package]
+      params[:ratings] = list[:ratings] if list.has_key?(:ratings)
+      params[:sort_by] = list[:sort_by] if list.has_key?(:sort_by)
+    end
+
     if params.has_key?(:ratings)
+      session[:ratings] = params[:ratings]
+
       selected_ratings = params[:ratings]
       selected_ratings.each do |key, content|
         @rating_flag[key] = true
         @movies += Movie.find_all_by_rating(key)
       end
+    elsif session.has_key?(:ratings)
+      redirect_flag = true
     else
       @all_ratings.each do |x|
         @rating_flag[x] = true
+        session[:ratings][x] = true
       end
       @movies = Movie.all
     end
 
-	if params[:sort_by] == 'title'
-      @movies = @movies.sort_by do |x|
-        x.title
+    if params.has_key?(:sort_by)
+      if params[:sort_by] == 'title'
+        session[:sort_by] = params[:sort_by]
+        @movies = @movies.sort_by do |x|
+          x.title
+        end
+        @flag = 'title'
+      elsif params[:sort_by] == 'date'
+        session[:sort_by] = params[:sort_by]
+        @movies = @movies.sort_by do |x|
+          x.release_date
+        end
+        @flag = 'date'
       end
-      @flag = 'title'
-    elsif params[:sort_by] == 'date'
-      @movies = @movies.sort_by do |x|
-        x.release_date
-      end
-      @flag = 'date'
-	end
+    elsif session.has_key?(:sort_by)
+      redirect_flag = true
+    end
     
+    if redirect_flag
+      redirect_to movies_path(:data_package => session)
+    end
   end
 
   def new
